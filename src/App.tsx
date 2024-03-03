83d89fbd31f6dc7fafeda18174d87de0cc158b95
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-03-03 02:18:53 +0100

    Use the new sjf scheduler

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,47 @@ import useSjfScheduler from './hooks/useSjfScheduler';
 import toSecond from './utils/toSecond';
 
 const App = () => {
-  const { processes, time, isPaused, spawnProcess, toggle } = useSjfScheduler();
+  const {
+    processes,
+    time,
+    isPaused,
+    spawnProcess,
+    toggle,
+    isPreemptive,
+    setIsPreemptive,
+  } = useSjfScheduler();
 
   return (
     <div>
       <h1>SJF Scheduler</h1>
       <p>Time: {toSecond(time)}</p>
+      <label htmlFor="is-preemptive">Preemptive?</label>
+      <input
+        type="checkbox"
+        id="is-preemptive"
+        checked={isPreemptive}
+        onChange={() => setIsPreemptive(!isPreemptive)}
+      />
       <button onClick={toggle}>{isPaused ? 'Play' : 'Pause'}</button>
       <button onClick={spawnProcess}>Spawn process</button>
       <ul>
         <li>
           <span>PID</span>
           <span>Start</span>
+          <span>Initial Start</span>
           <span>Duration</span>
+          <span>Status</span>
         </li>
         {!processes.length ? (
           <p>No processes.</p>
         ) : (
-          processes.map(({ pid, start, duration }) => (
+          processes.map(({ pid, start, initialStart, duration, status }) => (
             <li key={pid}>
               <span>{pid}</span>
-              <span>{toSecond(start, 1)}</span>
-              <span>{toSecond(duration, 1)}</span>
+              <span>{toSecond(start)}</span>
+              <span>{toSecond(initialStart)}</span>
+              <span>{toSecond(duration)}</span>
+              <span>{status}</span>
             </li>
           ))
         )}
--- src/hooks/useSjfScheduler.tsx
+++ src/hooks/useSjfScheduler.tsx
@@ -1,38 +1,17 @@
 import { useEffect, useRef, useState } from 'react';
 import useTime from './useTime';
-import { PriorityQueue } from '@datastructures-js/priority-queue';
 import IProcess from '../types/process';
 import randbetween from '../utils/randbetween';
-
-interface ISjfScheduler {
-  idCount: number;
-  processes: PriorityQueue<IProcess>;
-  dequeue: () => IProcess | null;
-  getCurrentProcess: () => IProcess | null;
-  createProcess: (start: number, duration: number) => void;
-}
+import { ISjfScheduler, createSjfScheduler } from '../schedulers/sjf';
 
 const useSjfScheduler = () => {
-  const [time, toggle, isPaused, deltaTime] = useTime();
+  const [time, toggle, isPaused] = useTime();
+  const [isPreemptive, setIsPreemptive] = useState(false);
   const schedulerRef = useRef<ISjfScheduler | null>();
   const [processes, setProcesses] = useState<IProcess[]>([]);
 
   useEffect(() => {
-    schedulerRef.current = {
-      idCount: 0,
-      processes: new PriorityQueue<IProcess>((a, b) => a.duration - b.duration),
-      createProcess: function (start, duration) {
-        this.processes.enqueue({ pid: this.idCount, duration, start });
-        this.idCount++;
-      },
-      dequeue: function () {
-        if (this.getCurrentProcess() === null) return null;
-        return this.processes.dequeue();
-      },
-      getCurrentProcess: function () {
-        return this.processes.front();
-      },
-    };
+    schedulerRef.current = createSjfScheduler();
   }, []);
 
   useEffect(() => {
@@ -42,24 +21,65 @@ const useSjfScheduler = () => {
     const currentProcess = scheduler.getCurrentProcess();
     if (!currentProcess) return;
 
-    currentProcess.duration -= deltaTime;
-    if (currentProcess.duration <= 0) scheduler.dequeue();
+    if (currentProcess.responseTime === -1) {
+      // calculate response time (the time between when the process arrived in
+      // the system and the first time it was processed)
+      currentProcess.responseTime = time - currentProcess.initialStart;
+    }
 
-    setProcesses(scheduler.processes.toArray());
-  }, [deltaTime]);
+    currentProcess.status = 'run';
+    currentProcess.duration -= time - currentProcess.start;
+    currentProcess.start = time;
+    if (currentProcess.duration <= 0) {
+      scheduler.dequeue();
+      const nextProcess = scheduler.getCurrentProcess();
+      if (nextProcess) nextProcess.start = time;
+    }
+
+    setProcesses(scheduler.getProcesses());
+  }, [time, isPaused]);
 
   const spawnProcess = () => {
     const scheduler = schedulerRef.current;
     if (!scheduler) return;
 
+    const prevProcess = scheduler.getCurrentProcess();
     scheduler.createProcess(
       Math.ceil(time),
       Math.floor(randbetween(2, 10)) * 1000
     );
-    setProcesses(scheduler.processes.toArray());
+
+    if (prevProcess !== null && prevProcess !== scheduler.getCurrentProcess()) {
+      // if there was a previous process but with pre-emptive it was pushed back
+      // then change its status from running to ready
+      prevProcess.status = 'ready';
+    }
+
+    setProcesses(scheduler.getProcesses());
   };
 
-  return { spawnProcess, toggle, isPaused, processes, time };
+  const setPreemptive = (isPreemptive: boolean) => {
+    const scheduler = schedulerRef.current;
+    if (!scheduler) return;
+
+    setIsPreemptive((prevIsPreemptive) => {
+      if (prevIsPreemptive === isPreemptive) return prevIsPreemptive;
+
+      scheduler.setIsPreemptive(isPreemptive);
+      setProcesses(scheduler.getProcesses());
+      return isPreemptive;
+    });
+  };
+
+  return {
+    spawnProcess,
+    setIsPreemptive: setPreemptive,
+    isPreemptive,
+    toggle,
+    isPaused,
+    processes,
+    time,
+  };
 };
 
 export default useSjfScheduler;
