9ba9138546172262cff520d101e9ee4da46f57a7
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-03-16 19:10:39 +0100

    Create home component

---
+++ src/components/Home.tsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <>
+      <h1 className="text-4xl mt-2">Welcome to scheduler emulations!</h1>
+      <p className="my-2">
+        Emulate scheduling policies such as Shortest-Job-First (SJF) or
+        Round-Robin (RR). To start, click on one of the scheduling policies in
+        the main navigation bar above.
+      </p>
+    </>
+  );
+};
+
+export default Home;
