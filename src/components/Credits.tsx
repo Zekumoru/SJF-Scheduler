a00b11ca700c6e446ca667bd2c26483b7f2f1848
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-03-17 00:13:09 +0100

    Style footer

--- src/components/Credits.tsx
+++ src/components/Credits.tsx
@@ -1,17 +1,25 @@
 const Credits = () => {
   return (
-    <p className="max-w-screen-xl mx-auto p-4">
-      Favicon from{' '}
-      <a
-        href="https://www.flaticon.com/free-icons/process"
-        title="process icons"
-        rel="noreferrer noopener"
-      >
-        Freepik
-      </a>{' '}
-      | Created by <a href="https://github.com/Zekumoru/schedulers">Zekumoru</a>
-      .
-    </p>
+    <div className="flex gap-2 max-w-screen-xl mx-auto p-4 flex-col md:flex-row md:gap-4">
+      <p className="flex-1">
+        Favicon from{' '}
+        <a
+          href="https://www.flaticon.com/free-icons/process"
+          title="process icons"
+          rel="noreferrer noopener"
+          className="underline"
+        >
+          Freepik
+        </a>
+      </p>
+      <p className="flex-1">
+        Created by{' '}
+        <a className="underline" href="https://github.com/Zekumoru/schedulers">
+          Zekumoru
+        </a>
+        .
+      </p>
+    </div>
   );
 };
 
