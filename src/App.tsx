b77acd9970620ff1b0e62ffc8f05b800619e56a7
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-03-16 19:19:46 +0100

    Implement routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import MainNavBar from './components/MainNavBar';
+import SjfScheduler from './components/SjfScheduler';
 import RRScheduler from './components/RRScheduler';
 
 const App = () => {
   return (
-    <main>
-      <RRScheduler />
-    </main>
+    <BrowserRouter>
+      <MainNavBar />
+      <main className="max-w-screen-xl mx-auto p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sjf-scheduler" element={<SjfScheduler />} />
+          <Route path="/rr-scheduler" element={<RRScheduler />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 };
 
