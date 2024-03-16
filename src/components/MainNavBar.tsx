439ce9507f533ffa00075c290e5a0a73e0445f16
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-03-16 19:10:34 +0100

    Create main navigation component

---
+++ src/components/MainNavBar.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from 'react';
+import { Link } from 'react-router-dom';
+
+const MainNavItem = ({ children }: { children: ReactNode }) => {
+  return (
+    <li className="py-4 px-8 hover:bg-black cursor-pointer font-bold">
+      {children}
+    </li>
+  );
+};
+
+const MainNavBar = () => {
+  return (
+    <nav className="bg-neutral-950" aria-label="main navigation">
+      <ul className="flex max-w-[1200px] mx-auto flex-col md:flex-row">
+        <Link to="/">
+          <MainNavItem>Home</MainNavItem>
+        </Link>
+        <Link to="/sjf-scheduler">
+          <MainNavItem>SJF Scheduler</MainNavItem>
+        </Link>
+        <Link to="/rr-scheduler">
+          <MainNavItem>RR Scheduler</MainNavItem>
+        </Link>
+      </ul>
+    </nav>
+  );
+};
+
+export default MainNavBar;
